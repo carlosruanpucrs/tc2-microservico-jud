29c816bc3838daecca364273b66067985487dc7b
Carlos Ruan <c.ruan@edu.pucrs.br>
2025-11-22 22:05:48 -0300

    fix/bloqueio-jud: ajuste

--- src/main/java/com/carlosruanpucrs/tc2_microservico_jud/config/RabbitConfig.java
+++ src/main/java/com/carlosruanpucrs/tc2_microservico_jud/config/RabbitConfig.java
@@ -4,6 +4,9 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -50,5 +53,13 @@ public class RabbitConfig {
         return BindingBuilder.bind(judBloqueioConfirmacaoQueue())
                 .to(judBloqueioConfirmacaoExchange())
                 .with(JUD_BLOQUEIO_CONFIRMACAO_RK);
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(new Jackson2JsonMessageConverter());
+        return factory;
     }
 }
